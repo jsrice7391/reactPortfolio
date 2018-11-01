8e27f66ffb0ca19aa40266199c6ce322c4665123
Justin Rice <jsrice7391@gmail.com>
2018-10-31 22:18:07 -0400

    Added a photo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,12 @@ import React, { Component } from "react";
 import {Grid, Row, Col, Button, FormControl} from "react-bootstrap";
 import {SampleComp} from "../../components/SampleComp";
 import {NavMaster} from "../../components/NavMaster";
+import { pbkdf2Sync } from "crypto";
 
-
+const pStyle = {
+  height:"600px",
+  width:"100%"
+};
 
 class All extends Component {
 
@@ -16,7 +20,10 @@ class All extends Component {
 
   render(){
     return(
+      <div>
       <NavMaster/>
+        <img src="https://s3.us-east-2.amazonaws.com/friend-finder333/markus-spiske-445253-unsplash.jpg" style={pStyle}/>
+      </div>
 
     )
   }
